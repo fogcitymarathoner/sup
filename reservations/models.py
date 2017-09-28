6649a8f32d0ff356561dca036c66a4e57fd5a362
fogcitymarathoner <marc@fogtest.com>
2017-09-27 22:14:55 -0700

    Added models for Tables and Reservations

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Table(models.Model):
+
+    tableName = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "Table: %s %s" % (self.id, self.tableName)
+
+
+class Reservation(models.Model):
+
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    reservingPerson = models.CharField(max_length=30)
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+
+    def __str__(self):
+        return "Reservation: %s %s %s %s - %s" % (
+            self.id,
+            self.table,
+            self.reservingPerson,
+            self.startTime,
+            self.endTime
+        )
